d628cdaf76b7cbb81600a26db76ef10190e260b8
jwchen <jwchen.page@foxmail.com>
2022-01-21 00:01:58 +0800

    22-1-21 剑指offer  包含 min 函数的栈

---
+++ min_stack.py
@@ -0,0 +1,17 @@
+
+
+class MinStack:
+    def __init__(self):
+        pass
+
+    def push(self, x:int)->None:
+        pass
+
+    def pop(self) -> None:
+        pass
+
+    def top(self)->int:
+        pass
+
+    def min(self)->int:
+        pass
